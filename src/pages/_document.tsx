e025a991460bb8a3d89a6f1f0a93189d4e87d3df
GiovaniMenegat <gi.menegat10@gmail.com>
2022-10-17 14:40:07 -0300

    Revert "fix: env metatag"
    This reverts commit c5adc469bb48b8f5d360e56621e7ee31f3e253fa.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,7 +10,7 @@ export default class MyDocument extends Document {
           
                     <link rel="shortcut icon" href="/mekane-alternativa.png" type="image/png" />
 
-                    <meta name="facebook-domain-verification" content={`${process.env.FACEBOOK_ID}`} />
+                    <meta name="facebook-domain-verification" content="d0udac0oa4jax42bpsuzovdm6wh6n6" />
                 </Head>
                 <body>
                     <noscript><iframe src={`https://www.googletagmanager.com/ns.html?id=${process.env.GTM_ID}`} height="0" width="0" 
