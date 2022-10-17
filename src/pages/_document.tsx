cbd23da1afeab6b6d19c32308adc37b9371cd127
GiovaniMenegat <gi.menegat10@gmail.com>
2022-10-17 14:29:17 -0300

    feat: facebook meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,8 @@ export default class MyDocument extends Document {
                     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;800&display=swap" rel="stylesheet"/>
           
                     <link rel="shortcut icon" href="/mekane-alternativa.png" type="image/png" />
+
+                    <meta name="facebook-domain-verification" content="d0udac0oa4jax42bpsuzovdm6wh6n6" />
                 </Head>
                 <body>
                     <noscript><iframe src={`https://www.googletagmanager.com/ns.html?id=${process.env.GTM_ID}`} height="0" width="0" 
