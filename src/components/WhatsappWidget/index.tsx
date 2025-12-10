896d39b6cbb69408842545ea542dbdfe1512a611
Giovani Menegat <gi.menegat10@gmail.com>
2025-12-10 16:14:33 -0300

    update whats number

--- src/components/WhatsappWidget/index.tsx
+++ src/components/WhatsappWidget/index.tsx
@@ -4,7 +4,7 @@ export function WhatsappWidget() {
 
     return (
         <div className={styles.container}>
-            <a href="https://wa.me/555133199649" target="_blank" rel="noopener noreferrer">
+            <a href="https://wa.me/555130699836" target="_blank" rel="noopener noreferrer">
                 <img 
                     src="/images/whatsapp.png" 
                     alt="Whatsapp"
